83f479d6e99374f37839fe1a6de8b5f711f62568
danromf <dan.romanoff15@gmail.com>
2024-05-21 16:04:06 +0700

    layout profile screen

---
+++ ImageFeed/Profile/PofileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PofileViewController.swift
+//  ImageFeed
+//
+//  Created by Даниил Романов on 21.05.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+}
