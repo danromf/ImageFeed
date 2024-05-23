adf599e9feee0c014b069848ddfec0498b234ba4
danromf <dan.romanoff15@gmail.com>
2024-05-23 21:18:17 +0700

    layout SingleImage screen

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Даниил Романов on 21.05.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return } // 1
+            imageView.image = image // 2
+        }
+    }
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+}
